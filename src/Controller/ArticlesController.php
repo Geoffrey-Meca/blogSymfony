1e27eb318d3289fb08c4198f8eeb41e0a7b120e0
Reurti13 <82392871+Reurti13@users.noreply.github.com>
2022-10-04 16:15:10 +0200

    first commit

---
+++ src/Controller/ArticlesController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Comment;
+use App\Entity\Post;
+use App\Form\CommentType;
+use App\Form\PostType;
+use App\Repository\PostRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticlesController extends AbstractController
+{
+    #[Route('/articles', name: 'app_articles')]
+    public function index(PostRepository $postRepository): Response
+    {
+        $posts = $postRepository->findAll();
+
+        return $this->render('articles/index.html.twig', [
+            'posts' => $posts
+        ]);
+    }
+
+    #[Route('/posts/{id}', name: 'show_post')]
+    public function show(Post $post, Request $request, EntityManagerInterface $em)
+    {
+        $comment = new Comment;
+        $form = $this->createForm(CommentType::class, $comment);
+
+        $comment->setCreatedAt(new \DateTime());
+        $comment->setPost($post);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($comment);
+            $em->flush();
+        }
+
+        return $this->render('articles/post.html.twig', [
+            'post' => $post,
+            "form" => $form->createView()
+        ]);
+    }
+
+    #[route('/addArticle', name: 'add_article')]
+    public function addArticle(Post $post, Request $request, EntityManagerInterface $em)
+    {
+        $article = new Post;
+        $form = $this->createForm(PostType::class, $article);
+        $article->setCreatedAt(new \DateTime());
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($article);
+            $em->flush();
+            return $this->show($post->id, $request, $em);
+        }
+
+        return $this->render('articles/addArticle.html.twig', [
+            "form" => $form->createView()
+        ]);
+    }
+}
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,47 +2,17 @@
 
 namespace App\Controller;
 
-use App\Entity\Comment;
-use App\Entity\Post;
-use App\Form\CommentType;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Repository\PostRepository;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Request;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(PostRepository $postRepository): Response
+    public function index(): Response
     {
-        $posts = $postRepository->findAll();
 
-        return $this->render('home/index.html.twig', [
-            'posts' => $posts
-        ]);
-    }
-
-    #[Route('/posts/{id}', name: 'show_post')]
-    public function show(Post $post, Request $request, EntityManagerInterface $em)
-    {
-        $comment = new Comment;
-        $form = $this->createForm(CommentType::class, $comment);
-
-        $comment->setCreatedAt(new \DateTime());
-        $comment->setPost($post);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em->persist($comment);
-            $em->flush();
-        }
-
-        return $this->render('home/post.html.twig', [
-            'post' => $post,
-            "form" => $form->createView()
-        ]);
+        return $this->render('home/index.html.twig', []);
     }
 }
